fe14b0299042c5a7bb029d260057b5ef8f9d2a0d
quejiajiu <775773863@qq.com>
2017-04-06 10:36:29 +0800

    index01

---
+++ js/compare.js
@@ -0,0 +1,50 @@
+ var vm = new Vue({
+    el: '#app',
+    data: {
+        stateArr: [
+           {
+            isActive:true,
+            opts: []
+           },
+           {
+            isActive:true,
+            opts: []
+           },
+           {
+            isActive:true,
+            opts: []
+          }
+       ],
+       productList:[],
+       newTodo:{id:null,productImage:'',productName:''}
+    },
+    mounted: function () {
+      // var _this = this;
+      this.cartView();
+    },
+    methods: {
+      cartView: function () {
+        this.$http.get("aa.json").then(function(response){
+          var res = eval (response.data);
+          if(res && res.status=="1"){
+            this.productList = res.result.list;
+          }
+          
+          console.log(res.result.list[0].productImage);
+        },function(err){
+          console.log(err);
+        });
+      },
+      deleteTodo:function(index){
+         this.productList.splice(index,1);
+      },
+      addTodo:function (newTodo) {
+                this.productList.push(newTodo);
+                this.newTodo = {id:null,title:'',completed:false};
+            },
+
+    }
+
+  });
+
+ //console.log(vm.methods);
